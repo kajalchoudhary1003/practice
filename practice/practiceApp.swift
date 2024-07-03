335ef9f52477eda9f1dec00c05d367fc1a100f6f
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-07-03 13:55:17 +0530

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Kajal Choudhary on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
